28a5e904ac6cafd617a26638df4d38e9ee1775ed
Sadjad Fouladi <sfouladi@gmail.com>
2020-05-13 16:05:34 -0700

    dump_partials_cloud.sh: Parallel build for chunks + clean up the code.

--- scripts/dump_partials_cloud.sh
+++ scripts/dump_partials_cloud.sh
@@ -4,23 +4,21 @@ set -e
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
-if [ "$#" -ne 4 ]; then
-    echo "$0 PBRT_BIN INSTANCE_DIR CHUNK_DIR OUT_DIR"
+if [ "$#" -ne 5 ]; then
+    echo "Usage: $0 ACTION PBRT_BIN INSTANCE_DIR CHUNK_DIR OUT_DIR"
+    echo "  Valid actions: prepare-instances"
+    echo "                 prepare-chunks"
     exit 1
 fi
 
-PBRT_BIN=`readlink -f $1`
-INSTANCE_DIR=`readlink -f $2`
-CHUNK_DIR=`readlink -f $3`
-OUT_DIR=`readlink -f $4`
+ACTION="$1"
+PBRT_BIN=`readlink -f $2`
+INSTANCE_DIR=`readlink -f $3`
+CHUNK_DIR=`readlink -f $4`
+OUT_DIR=`readlink -f $5`
 
-# Preparation
-
-if [ -d "${OUT_DIR}" ] && [ -n "$(ls -A "${OUT_DIR}")" ]
-then
-  echo "Error: ${OUT_DIR} is not empty."
-  exit 1
-fi
+INSTANCE_TARGETS="${OUT_DIR}/instance-targets.gg"
+CHUNK_TARGETS="${OUT_DIR}/chunk-targets.gg"
 
 echo -n "> Creating ${OUT_DIR}... "
 mkdir -p "${OUT_DIR}"
@@ -29,16 +27,14 @@ echo "done."
 cd "${OUT_DIR}"
 gg init
 
-# Stage I (dumping instances)
+function prepare_instances {
+  gg_pipe=$(mktemp -u)
+  mkfifo "${gg_pipe}"
+  gg repl <"${gg_pipe}" &
 
-## I.1 Generating thunks
+  exec 3>"${gg_pipe}"
 
-gg_pipe=$(mktemp -u)
-mkfifo "${gg_pipe}" # safe; this would fail if a file with this name exists
-gg repl <"${gg_pipe}" &
-
-# add the basic entities
-cat >>"${gg_pipe}" <<EOT
+  cat >&3 <<EOT
 push executable ${PBRT_BIN}
 push arg pbrt
 push arg --nthreads=1
@@ -53,34 +49,127 @@ push output MAT0
 push output MAT1
 EOT
 
-target_thunks="${OUT_DIR}/targets.gg"
+  : >"${INSTANCE_TARGETS}"
 
-for pbrt_file in $(ls "${INSTANCE_DIR}")
-do
-  name="`basename ${pbrt_file} .pbrt`"
-  output_dir="${OUT_DIR}/${name}"
-  mkdir -p "${output_dir}"
+  for pbrt_file in $(ls "${INSTANCE_DIR}")
+  do
+    name="`basename ${pbrt_file} .pbrt`"
+    output_dir="${OUT_DIR}/${name}"
+    mkdir -p "${output_dir}"
 
-  {
-    echo "push data ${INSTANCE_DIR}/${pbrt_file}"
-    echo "push link input.pbrt ${INSTANCE_DIR}/${pbrt_file}"
-    echo "create thunk"
-    echo "create placeholder T0 ${output_dir}/T0"
-    echo "create placeholder HEADER ${output_dir}/HEADER"
-    echo "create placeholder MANIFEST ${output_dir}/MANIFEST"
-    echo "create placeholder MAT0 ${output_dir}/MAT0"
-    echo "create placeholder MAT1 ${output_dir}/MAT1"
-    echo "pop data"
-    echo "pop link"
-  } >>"${gg_pipe}"
+    {
+      echo "push data ${INSTANCE_DIR}/${pbrt_file}"
+      echo "push link input.pbrt ${INSTANCE_DIR}/${pbrt_file}"
+      echo "create thunk"
+      echo "create placeholder T0 ${output_dir}/T0"
+      echo "create placeholder HEADER ${output_dir}/HEADER"
+      echo "create placeholder MANIFEST ${output_dir}/MANIFEST"
+      echo "create placeholder MAT0 ${output_dir}/MAT0"
+      echo "create placeholder MAT1 ${output_dir}/MAT1"
+      echo "pop data"
+      echo "pop link"
+    } >&3
 
-  {
-    echo "${output_dir}/T0"
-    echo "${output_dir}/HEADER"
-    echo "${output_dir}/MANIFEST"
-    echo "${output_dir}/MAT0"
-    echo "${output_dir}/MAT1"
-  } >>"${target_thunks}"
-done
+    {
+      echo "${output_dir}/T0"
+      echo "${output_dir}/HEADER"
+      echo "${output_dir}/MANIFEST"
+      echo "${output_dir}/MAT0"
+      echo "${output_dir}/MAT1"
+    } >>"${INSTANCE_TARGETS}"
+  done
 
-wait # wait for gg repl to die
+  echo "exit" >>"${gg_pipe}"
+  exec 3>&-
+  wait # wait for gg repl to die
+}
+
+function prepare_chunks {
+  instance_archive="instance-headers.tar"
+  find . -type f -name HEADER | tar cf ${instance_archive} -T -
+
+  entry_program=$(mktemp)
+  cat >>"${entry_program}" <<EOT
+#!/bin/sh
+
+mkdir -p proxies
+/bin/tar -C proxies -xf ${instance_archive}
+./pbrt --nthreads=1 --nomaterial --proxydir proxies --dumpscene . input.pbrt
+rm -rf proxies
+EOT
+
+  gg_pipe=$(mktemp -u)
+  mkfifo "${gg_pipe}"
+  gg repl <"${gg_pipe}" &
+
+  exec 3>"${gg_pipe}"
+
+  # add the basic entities
+  cat >&3 <<EOT
+push executable ${entry_program}
+push executable ${PBRT_BIN}
+push arg entry-program
+push output T0
+push output HEADER
+push output MANIFEST
+push output LIGHTS
+push output MAT0
+push output MAT1
+push data ${instance_archive}
+push link ${instance_archive} ${instance_archive}
+push link pbrt ${PBRT_BIN}
+EOT
+
+  : >"${CHUNK_TARGETS}"
+
+  for pbrt_file in $(ls "${CHUNK_DIR}")
+  do
+    name="`basename ${pbrt_file} .pbrt`"
+    output_dir="${OUT_DIR}/${name}"
+    mkdir -p "${output_dir}"
+
+    {
+      echo "push data ${CHUNK_DIR}/${pbrt_file}"
+      echo "push link input.pbrt ${CHUNK_DIR}/${pbrt_file}"
+      echo "create thunk"
+      echo "create placeholder T0 ${output_dir}/T0"
+      echo "create placeholder HEADER ${output_dir}/HEADER"
+      echo "create placeholder MANIFEST ${output_dir}/MANIFEST"
+      echo "create placeholder LIGHTS ${output_dir}/LIGHTS"
+      echo "create placeholder MAT0 ${output_dir}/MAT0"
+      echo "create placeholder MAT1 ${output_dir}/MAT1"
+      echo "pop data"
+      echo "pop link"
+    } >&3
+
+    {
+      echo "${output_dir}/T0"
+      echo "${output_dir}/HEADER"
+      echo "${output_dir}/MANIFEST"
+      echo "${output_dir}/LIGHTS"
+      echo "${output_dir}/MAT0"
+      echo "${output_dir}/MAT1"
+    } >>${CHUNK_TARGETS}
+  done
+
+  echo "exit" >>"${gg_pipe}"
+  exec 3>&-
+  wait # wait for gg repl to die
+}
+
+case $ACTION in
+
+  prepare-instances)
+    prepare_instances
+    ;;
+
+  prepare-chunks)
+    prepare_chunks
+    ;;
+
+  *)
+    echo "Invalid action: $ACTION"
+    exit 1
+    ;;
+
+esac
