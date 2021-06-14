a6146073e313e35ac6e955d64726a7645e935d1d
Sadjad Fouladi <sfouladi@gmail.com>
2021-06-14 01:17:38 +0000

    Implement LiteRecordReader.

---
+++ src/messages/lite.cpp
@@ -0,0 +1,25 @@
+#include "lite.h"
+
+using namespace std;
+
+LiteRecordReader::LiteRecordReader(const char* buffer, const size_t len)
+    : buffer_(buffer), len_(len), end_(buffer_ + len_) {}
+
+bool LiteRecordReader::read(const char** out_buffer, size_t* out_len) {
+    // first 4 bytes are the record length
+    if (buffer_ + 4 > end_) {
+        return false;
+    }
+
+    const uint32_t* record_len =
+        reinterpret_cast<const uint32_t*>(buffer_);
+
+    if (out_len != nullptr) {
+        *out_len = *record_len;
+    }
+
+    *out_buffer = buffer_ + 4;
+    buffer_ += (*record_len) + 4;
+
+    return buffer_ <= end_;
+}
---
+++ src/messages/lite.h
@@ -0,0 +1,42 @@
+#ifndef PBRT_MESSAGES_LITE_H
+#define PBRT_MESSAGES_LITE_H
+
+#include <fstream>
+#include <stdexcept>
+#include <string>
+
+class LiteRecordReader {
+  public:
+    LiteRecordReader(const char* buffer, const size_t len);
+
+    bool read(const char** buf, size_t* len);
+
+    template <class T>
+    bool read(T* t);
+
+    bool eof() const { return buffer_ == end_; }
+
+  private:
+    const char* buffer_;
+    const size_t len_;
+    const char* end_;
+};
+
+template <class T>
+bool LiteRecordReader::read(T* t) {
+    size_t out_len;
+    const char* ptr;
+    bool result = read(&ptr, &out_len);
+
+    if (out_len != sizeof(T)) {
+        throw std::runtime_error(std::string("unexpected size: expected ") +
+                                 std::to_string(sizeof(T)) + ", got " +
+                                 std::to_string(out_len));
+    }
+
+    *t = *reinterpret_cast<const T*>(ptr);
+
+    return result;
+}
+
+#endif /* PBRT_MESSAGES_LITE_H */
