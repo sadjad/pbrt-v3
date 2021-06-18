6403e6cc0079135eeed28538622758631b2b1135
Sadjad Fouladi <sfouladi@gmail.com>
2021-06-18 12:25:57 -0700

    treeletdumpbvh.cpp: Fix a bug related to dumping faceIndices.

--- src/cloud/treeletdumpbvh.cpp
+++ src/cloud/treeletdumpbvh.cpp
@@ -1989,7 +1989,7 @@ vector<uint32_t> TreeletDumpBVH::DumpTreelets(bool root) const {
             vector<Vector3f> S(numVerts);
             vector<Normal3f> N(numVerts);
             vector<Point2f> uv(numVerts);
-            vector<int> faceIdxs(numVerts);
+            vector<int> faceIdxs(numTris);
 
             for (size_t i = 0; i < numTris; i++) {
                 size_t triNum = triNums[i];
